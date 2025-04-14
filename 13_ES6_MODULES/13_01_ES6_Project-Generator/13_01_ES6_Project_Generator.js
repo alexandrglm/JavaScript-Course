f45ba3d649812aa24f0e96d9315cf9926e6f0a75
Alexandr Gomez <alexandrglm@users.noreply.github.com>
2025-04-14 12:10:39 +0200

    Started Module 13, NPM, Node, ES6 Modules

---
+++ 13_ES6_MODULES/13_01_ES6_Project-Generator/13_01_ES6_Project_Generator.js
@@ -0,0 +1,22 @@
+/*
+1. Set up NMP (done)
+2. Import devcamp-js-builder -g (GLOBALLY!) (may be required to import it globally as root, try to avoid this when posible)
+3. Start by typing js-generate. A "new creation" menu will be shown:
+
+? What project template would you like to generate? (Use arrow keys)
+❯ es6-starter 
+  react-bootstrap 
+  react-redux 
+  react-redux-router 
+  react-router 
+  react-skeleton 
+  react-typescript 
+
+
+4. We've created a simple ES6-Starter at ../13_00_testNode/
+
+5. Close, into ./src/ we got bootstrap.js (index.js).
+
+6. Initialize the possible dependences by `npm install`
+*/
+
