baace33499230dc787a93710dd9ed1a90f68e353
Alexandr Gomez <alexandrglm@users.noreply.github.com>
2025-05-28 11:16:58 +0200

    Updated

---
+++ 16_DOM/16_01_DOM_intro/16_01_DOM_intro.js
@@ -0,0 +1,16 @@
+//
+
+const user = {
+    name: 'Kristine',
+    email: 'kristine@devcamp.com',
+    favorites: [
+        'Chipotle',
+        'Chik fil a'
+  ]
+}
+
+
+// 1
+const ejemploUno = user;
+
+console.log(user)
